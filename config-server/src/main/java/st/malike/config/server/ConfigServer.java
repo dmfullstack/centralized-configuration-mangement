24550c00c3d82b4408f5a227b47e57cdf6f58803
malike <st.malike@gmail.com>
2018-05-20 06:53:37 +0000

    configuration server

---
+++ config-server/src/main/java/st/malike/config/server/ConfigServer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package st.malike.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ *
+ * @author malike_st
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SpringApplication.run(ConfigServer.class, args);
+    }
+
+}
