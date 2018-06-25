c98c610aac82d57092b877cc8577a7371691338e
malike <st.malike@gmail.com>
2018-06-25 22:48:30 +0000

    search message service

--- message-summary/src/main/java/st/malike/message/summary/http/SummaryController.java
+++ message-summary/src/main/java/st/malike/message/summary/http/SummaryController.java
@@ -3,12 +3,14 @@ package st.malike.message.summary.http;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import st.malike.message.summary.service.MessageService;
 import st.malike.message.summary.util.Enums;
 import st.malike.message.summary.util.JSONResponse;
 
@@ -18,19 +20,21 @@ import st.malike.message.summary.util.JSONResponse;
 @Controller
 public class SummaryController extends ExceptionController {
 
+  @Autowired
+  private MessageService messageService;
+
   @RequestMapping(value = {
       "/message/search"}, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
-  public JSONResponse searchMessage(@RequestBody Object data, HttpServletResponse response,
-      HttpServletRequest request){
+  public JSONResponse searchMessage(@RequestBody Object data){
     JSONResponse jSONResponse = new JSONResponse();
     Map<String, Object> dataHash = (Map<String, Object>) data;
     String param = null;
     Integer offset = 0;
     Integer limit = 100;
     jSONResponse.setStatus(true);
-    jSONResponse.setCount(1);
-    jSONResponse.setResult(null);
+    jSONResponse.setCount(messageService.countSearchMessage(param));
+    jSONResponse.setResult(messageService.searchMessage(param,offset,limit));
     jSONResponse.setMessage(Enums.JSONResponseMessage.SUCCESS.toString());
     return jSONResponse;
   }
--- message-summary/src/main/java/st/malike/message/summary/service/MessageService.java
+++ message-summary/src/main/java/st/malike/message/summary/service/MessageService.java
@@ -1,8 +1,24 @@
 package st.malike.message.summary.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
 /**
  * @autor malike_st
  */
+@Service
 public class MessageService {
 
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
+  public int countSearchMessage(String param) {
+    return 0;
+  }
+
+  public List<Object> searchMessage(String param,Integer offset,Integer limit){
+    return null;
+  }
 }
