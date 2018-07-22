15337ddbdb58e0f1c4961cf1cb6735e1280c4b2d
malike <st.malike@gmail.com>
2018-07-22 23:24:14 +0000

    added context loading test

---
+++ message-summary/src/test/java/st/malike/message/summary/MessageSummaryMainTest.java
@@ -0,0 +1,17 @@
+package st.malike.message.summary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @autor malike_st
+ */
+@RunWith(SpringRunner.class)
+public class MessageSummaryMainTest {
+
+    @Test
+    public void contextLoads() {
+      MessageSummaryMain.main(new String[]{});
+    }
+  }
